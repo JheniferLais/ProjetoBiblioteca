425695909ab14253ed581710cedffd724ec320e1
João <joaogiatti2006@gmail.com>
2024-09-30 18:32:12 -0300

    feat: add func consultaDeLivros

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <locale.h>
+#include <ctype.h> // para o isdigit() e isalpha()
 
 using namespace std;
 
@@ -42,7 +43,7 @@ void cadastroDeLivros(Biblioteca livro[100]){
     cin >> ano;
     cout << "DIGITE A QUANTIDADE EM ESTOQUE DO LIVRO(min 1, max 10): ";
     cin >> estoque;
-    //VERIFICAÃ‡ÃƒO DE ESTOQUE
+    //VERIFICAÇÃO DE ESTOQUE
     if (estoque > 10 || estoque < 1) {
         cout << "QUANTIDADE INVALIDA!!!" << endl << "LIVRO NAO CADASTRADO";
     }
@@ -64,26 +65,97 @@ void cadastroDeLivros(Biblioteca livro[100]){
     }
 }
 //
-void consultaDeLivros(){
-	int comandoCDL = 1;
-	cout << "CONSULTA DE LIVROS" << endl;
-    cout << "------------------" << endl;
-    cout << "[1]. Listar todos os Livros" << endl;
-    cout << "[2]. Buscar livro" << endl;
-    cout << "[3]. Cancelar" << endl;
-    cin >> comandoCDL;
-    while(true){
-	    if(comandoCDL == 1){
-	    	for(int i = 0; i < livroIndex; i++){
-	    		cout << "Livro " << livroIndex << endl;
-			}
-		}else if(comandoCDL == 2){
-			
-		}else if(comandoCDL == 3){
-			break;
-		}
-	}
+void consultaDeLivros(Biblioteca livro[100]){
+	int comandoCDL;
+    bool continuar = true;
+
+    while(continuar){
+        cout << "CONSULTA DE LIVROS" << endl;
+        cout << "------------------" << endl;
+        cout << "[1]. Listar todos os Livros" << endl;
+        cout << "[2]. Buscar livro" << endl;
+        cout << "[3]. Cancelar" << endl;
+        cin >> comandoCDL;
+
+        if(comandoCDL == 1){
+            // LISTAR TODOS OS LIVROS
+            if(livroIndex == -1) {
+                cout << "Nenhum livro cadastrado!" << endl;
+            } else {
+                for(int i = 0; i <= livroIndex; i++){
+                    cout << "Livro " << i+1 << ": " << endl;
+                    cout << "ID: " << livro[i].id << endl;
+                    cout << "Título: " << livro[i].titulo << endl;
+                    cout << "Autor: " << livro[i].autor << endl;
+                    cout << "Número de Páginas: " << livro[i].paginas << endl;
+                    cout << "Estoque: " << livro[i].estoque << endl;
+                    cout << "------------------" << endl;
+                }
+            }
+        } else if(comandoCDL == 2){
+            // BUSCAR LIVRO POR TITULO OU ID
+            bool encontrado = false;
+            int i = 0;
+            int isNumeric = 1;
+            char buscaTituloId[100];
+            cout << "Digite o Título ou o ID do livro: ";
+            cin >> buscaTituloId;
+            
+            // VERIFICAÇÃO DE ENTRADA NÚMERICA (titulo ou id)
+            while (buscaTituloId[i] != '\0') {
+                if (!isdigit(buscaTituloId[i])) {
+                    isNumeric = 0;
+                    break;
+                }
+                i++;
+            }
+            
+            if (isNumeric) {
+                // BUSCA POR ID
+                for (int k = 0; k <= livroIndex; k++) {
+                    char idString[10]; // controle para armazenar o ID como string
+                    sprintf(idString, "%d", livro[k].id); // converte int para string
+                
+                    if (strcmp(idString, buscaTituloId) == 0) {
+                        cout << "Livro Encontrado!:\n";
+                        cout << "ID: " << livro[k].id << endl;
+                        cout << "Título: " << livro[k].titulo << endl;
+                        cout << "Autor: " << livro[k].autor << endl;
+                        cout << "Número de Páginas: " << livro[k].paginas << endl;
+                        cout << "Estoque: " << livro[k].estoque << endl;
+                        cout << "------------------" << endl;
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    cout << "Livro não encontrado por ID.\n";
+                }
+            } else {
+                // BUSCA POR TITULO
+                for (int k = 0; k <= livroIndex; k++) {
+                    if (strcmp(livro[k].titulo, buscaTituloId) == 0) {
+                        cout << "Livro Encontrado!:\n";
+                        cout << "ID: " << livro[k].id << endl;
+                        cout << "Título: " << livro[k].titulo << endl;
+                        cout << "Autor: " << livro[k].autor << endl;
+                        cout << "Número de Páginas: " << livro[k].paginas << endl;
+                        cout << "Estoque: " << livro[k].estoque << endl;
+                        cout << "------------------" << endl;
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    cout << "Livro não encontrado por título.\n";
+                }
+            }
+        } else if(comandoCDL == 3){
+            continuar = false;
+        }
+    }
 }
+
 int main(){
 	setlocale(LC_ALL, "Portuguese");
     Biblioteca livro[100]; //ARRAY QUE GUARDA AS INFORMAÃ‡OES COM TODOS OS LIVROS(max de 100 livros)
@@ -107,7 +179,7 @@ int main(){
             cadastroDeLivros(livro);
         }
         else if(opcao == 2){ //CONSULTA DE LIVROS
-            consultaDeLivros();
+            consultaDeLivros(livro);
         }
         else if(opcao == 3){ //EMPRÃ‰STIMO DE LIVROS
             //emprestimoDeLivros();
